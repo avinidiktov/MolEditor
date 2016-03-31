67b6becb9a842da963dac91e90ef938205af1f2a
avinidiktov <aavinidiktov@gmail.com>
2016-04-01 00:19:21 +0700

    draw rectangle in element of grid

--- js/script.js
+++ js/script.js
@@ -4,7 +4,26 @@ var ctx = myCanvas.getContext('2d');
 var canvasWidth = 800;
 var canvasHeight = 600;
 
+
+var xPos = 0, yPos = 0;
+
+
 var gridElem = [];
+
+myCanvas.addEventListener("click",click);
+function click(event){
+    if (event.type == "click") {
+        xPos = event.clientX;
+        yPos = event.clientY - 56;
+
+        console.log(xPos,yPos);
+        detectPosition(xPos, yPos);
+
+    }
+
+
+}
+
 
 function drawGrid(){
 
@@ -24,27 +43,39 @@ function drawGrid(){
 }
 
 
+function paintRect(x, y){
+
+    console.log(x * 40, y * 40);
+
+    ctx.fillRect(x*40,y*40, x+40, y+40);
+
+}
+
 function dividedCanvasOnGrid(){
 
-    for (var i = 40; i < canvasWidth; i+=40) { // width
-        for (var j = 40; j < canvasHeight; j+=40) {//height
-            el = new Object();
-            el.horiz = i;
-            el.vert = j;
+    for (var i = 0; i < canvasHeight; i+=40) { //height 600
+        for (var j = 0; j < canvasWidth; j+=40) {// width 800
+            el = {};
+            el.horiz = j;
+            el.vert = i;
             gridElem.push(el);
         }
     }
 }
 
-function detectPosition(pointX, pointY){
-    dividedCanvasOnGrid();
+function detectPosition(x, y){
+    var moduloX = Math.floor(x / 40);
+    var moduloY = Math.floor(y / 40);
 
-    var a = gridElem;
+    console.log(moduloX);
+    console.log(moduloY);
 
-    alert(gridElem[100].horiz);
+
+    paintRect(moduloX, moduloY);
+    //alert(gridElem.length);
 }
 
 
 
 drawGrid();
-detectPosition(0,0);
+dividedCanvasOnGrid();
